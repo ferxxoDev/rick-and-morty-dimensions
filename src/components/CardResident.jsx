2237fc3b9431d30ac11b9e8edb600037ecf6018e
ivanhack34 <ivanpolanco34@gmail.com>
2022-10-01 21:04:29 -0400

    Tercer parte del proyecto

--- src/components/CardResident.jsx
+++ src/components/CardResident.jsx
@@ -11,9 +11,28 @@ const CardResident = ({url}) => {
         .catch(err => console.log(err))
     }, [])
     
+    console.log(resident);
 
   return (
-    <div>CardResident</div>
+    <article>
+        <header>
+            <img src={resident?.image} alt="" />
+            <div>
+                <div className='circle'></div>
+                <span>{resident?.status}</span>
+            </div>
+        </header>
+
+        <section>
+            <h3>{resident?.name}</h3>
+            <ul>
+                <li><span>Species: </span>{resident?.species}</li>
+                <li><span>Origin: </span>{resident?.origin.name}</li>
+                <li><span>Episodes where appear: </span>{resident?.episode.length}</li>
+            </ul>
+        </section>
+
+    </article>
   )
 }
 
